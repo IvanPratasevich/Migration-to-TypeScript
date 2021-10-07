09040defc61a0c9986a410e39e7950ff56753ef8
IvanPratasevich <iv.pratasevich@gmail.com>
2021-10-07 15:27:26 +0300

    feat: add adaptive to some sections

--- museum-adaptive/gallery.js
+++ museum-adaptive/gallery.js
@@ -1,20 +1,20 @@
 const pictureInnerContainer = document.querySelector('.picture-inner-container');
 const images = [
-  `assets/images/galery/galery1.jpg`,
-  `assets/images/galery/galery2.jpg`,
-  `assets/images/galery/galery3.jpg`,
-  `assets/images/galery/galery4.jpg`,
-  `assets/images/galery/galery5.jpg`,
-  `assets/images/galery/galery6.jpg`,
-  `assets/images/galery/galery7.jpg`,
-  `assets/images/galery/galery8.jpg`,
-  `assets/images/galery/galery9.jpg`,
-  `assets/images/galery/galery10.jpg`,
-  `assets/images/galery/galery11.jpg`,
-  `assets/images/galery/galery12.jpg`,
-  `assets/images/galery/galery13.jpg`,
-  `assets/images/galery/galery14.jpg`,
-  `assets/images/galery/galery15.jpg`,
+  `assets/images/galery/galery1.webp`,
+  `assets/images/galery/galery2.webp`,
+  `assets/images/galery/galery3.webp`,
+  `assets/images/galery/galery4.webp`,
+  `assets/images/galery/galery5.webp`,
+  `assets/images/galery/galery6.webp`,
+  `assets/images/galery/galery7.webp`,
+  `assets/images/galery/galery8.webp`,
+  `assets/images/galery/galery9.webp`,
+  `assets/images/galery/galery10.webp`,
+  `assets/images/galery/galery11.webp`,
+  `assets/images/galery/galery12.webp`,
+  `assets/images/galery/galery13.webp`,
+  `assets/images/galery/galery14.webp`,
+  `assets/images/galery/galery15.webp`,
   ];
 function shuffle(images) {
   images.sort(() => Math.random() - 0.5);
@@ -25,6 +25,7 @@ function shuffle(images) {
       const img = document.createElement('img');
       img.classList.add('gallery-img')
       img.src = `${item}`;
+      img.loading = 'lazy';
       img.alt = 'galery' +`${item}`.replace(/\D+/g, '');
       pictureInnerContainer.append(img);
     });
